4e4051f1fa833e7d520d4451ac4125dc340b7b01
Solo <costudiolabor@gmail.com>
2021-11-12 01:30:56 +0300

    Добавил скрипты платформа шарик перемещение гейм контроллер чек шарика

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Game Controller
+/// </summary>
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    [Header("ссылка на игрока") ]
+    public GameObject player;
+
+    public CheckerAir checkerAirPlayer;
+
+    public int countAirPlayer
+    {
+        get
+        {
+            return checkerAirPlayer.countAir;
+        }
+       
+    }
+   
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        checkerAirPlayer = player.GetComponent<CheckerAir>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlatformForBallons.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformForBallons : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPlayer;
+
+    [SerializeField]
+    private GameObject currentPlayer;
+
+    [Header("Время между надувами воздуха")]
+    [SerializeField]
+    private float deltaTime = 2.0f;
+
+    [Header("Точка спавна шаров")]
+    [SerializeField]
+    private Transform pointSpawnBallon;
+
+    [Header("Обьект Шар")]
+    [SerializeField]
+    private GameObject gameObjectBaloon;
+
+
+
+    private void Update()
+    {
+
+    }
+
+
+    void InflatingBallons()
+    {
+        //Сколько воздуха забираем
+        int countAirDel = 1;
+        if (currentPlayer.GetComponent<CheckerAir>().DelAir(countAirDel))
+        {
+
+            gameObjectBaloon.transform.position = pointSpawnBallon.position;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            currentPlayer = collision.gameObject;
+            InvokeRepeating("InflatingBallons", 2, deltaTime);
+            isPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            CancelInvoke();
+            currentPlayer = null;
+            isPlayer = false;
+        }
+    }
+}
